db45bf2116d3121433e5895b86a6c26496a37f54
dilinjose <dilinjose34@gmail.com>
2022-04-01 22:54:40 +0530

    Updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+aim="Your perfect banking patner"
+acctName="Account Number"
+password="Password"
+acno=""
+pswd=""
 
+//database
+database={
+  1000:{acno:1000,username:"dilinjose",password:1000,balance:5000},
+  1001:{acno:1001,username:"dilinjose1",password:1001,balance:5000},
+  1002:{acno:1002,username:"dilinjose2",password:1002,balance:5000},
+}
   constructor() { }
 
   ngOnInit(): void {
   }
+ 
+  acnoChange(event:any){
+    this.acno=event.target.value
+  }
+  pswdChange(event:any){
+    this.pswd=event.target.value
+  }
 
+login(){
+  alert("Login clicked")
+}
 }
