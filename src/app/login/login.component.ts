72b35fd491ea1d23a8c191771a5092729f038e7b
dilinjose <dilinjose34@gmail.com>
2022-04-04 19:32:08 +0530

    update 04-04-2022

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,31 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-aim="Your perfect banking patner"
-acctName="Account Number"
-password="Password"
-acno=""
-pswd=""
+  aim = "Your perfect banking patner"
+  acctName = "Account Number"
+  password = "Password"
+  acno = ""
+  pswd = ""
 
-//database
-database={
-  1000:{acno:1000,username:"dilinjose",password:1000,balance:5000},
-  1001:{acno:1001,username:"dilinjose1",password:1001,balance:5000},
-  1002:{acno:1002,username:"dilinjose2",password:1002,balance:5000},
-}
+  //database
+  database : any= {
+    1000: { acno: 1000, username: "dilinjose", password: 1000, balance: 5000 },
+    1001: { acno: 1001, username: "dilinjose1", password: 1001, balance: 5000 },
+    1002: { acno: 1002, username: "dilinjose2", password: 1002, balance: 5000 },
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
- 
-  acnoChange(event:any){
-    this.acno=event.target.value
+
+  acnoChange(event: any) {
+    // console.log(event)
+    this.acno = event.target.value
   }
-  pswdChange(event:any){
-    this.pswd=event.target.value
+  pswdChange(event: any) {
+    this.pswd = event.target.value
   }
 
-login(){
-  alert("Login clicked")
-}
+  login() {
+    let acno = this.acno;
+    let pswd = this.pswd;
+
+    if (acno in this.database) {
+      if (pswd == this.database[acno]["password"]) {
+        alert("Login Successful")
+
+      }
+      else{
+        alert("Invalid Password")
+      }
+    }
+    else {
+      alert("Invalid Account Number");
+    }
+
+
+    // alert("Login clicked")1
+  }
 }
